97ae3eb67dcdd6b3b63760c20f7536246b974847
Muhammad Tabish <132977467+Raja-tabish@users.noreply.github.com>
2024-02-22 02:08:43 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,148 @@
+// Chapter no 1
+
+// Qno:1
+
+// alert("Welcome");
+
+// Qno:2
+
+// alert("Error! please enter a valid Password");
+
+// Qno:3
+
+// alert("Welcome to JS land... \nHappy coding");
+
+// Qno:4
+// alert("Welcome to JS Land...");
+// alert("Happy coding... \n prevent this page from creating additional dialogs.");
+
+
+// Qno:5
+
+// alert("Hello... i can run JS through my web Browser's console");
+
+// Chapter NO 2
+
+// Qno:1
+//  var Username="";
+
+// Qno:2
+// var myname="Muhammad Tabish";
+
+// Qno:3
+// var message="Hello world";
+// alert(message);
+
+// Qno:4
+// var studentname="Jhone Doe";
+// alert(studentname);
+
+// var age="15 years old";
+// alert(age);
+
+// var course="certified mobile application development"
+// alert(course);
+
+// Qno:5
+// alert("PIZZA \nPIZZ \nPIZ \nPI \nP");
+
+// Qno:6
+// var email="example@example.com";
+// alert(email);
+
+// Qno:7
+// var book="A smarter \n way to learn Javascript";
+// alert("i am trying to learn from the book "+ book);
+
+// Qno:8
+// document.write("Yah! i can write HTML content through JavaScript");
+
+// Qno:9
+// var design="▬▬▬▬▬▬▬▬▬ஜ۩۞۩ஜ▬▬▬▬▬▬▬▬▬";
+// alert(design);
+
+// ................CHAPTER NO 3
+
+// Qno:1
+// var age="I am 15 year old";
+// alert(age);
+
+// Qno:2
+// var webpage="14";
+// alert("You have visited this site "+webpage+" time")
+
+// Qno:3
+// var birthyear="1999";
+// document.write("My birth year is "+birthyear+ " <br>data type of my declaired variable is number");
+
+// Qno:4
+// var visitorname="<b> jhon doe</b>" , quantity="<b>5" , producttitle="T-Shirt(s)</b>";
+// document.write(visitorname +" ordered "+quantity +" " +producttitle+" on XYZclothing store");
+
+// ................word File.apply............
+// ............CHAPTER 1 (Alerts).............
+// Qno:1
+//  alert("First Name");
+
+// alert("Last Name");
+
+// alert("Email");
+
+// alert("Phone Number");
+
+// alert("Password");
+
+// Qno:2
+// alert("You're learning Javascript!");
+
+// Qno:3
+// alert("i love pakistan")
+
+// .............CHAPTER 2 (Veriables)..............
+// Qno:1
+// var firstName="Jhon";
+// alert(firstName);
+
+// Qno:2
+// var myVariable;
+// myVariable="helloworld";
+// alert(myVariable);
+
+// Qno:3
+// var teamName="Champions"
+// alert(teamName);
+
+// Qno:4
+// var bestMan = "Charlie";
+// bestMan = "albert";
+// alert(bestMan);
+
+// ...............CHAPTER 3 (Variables for numbers)..................
+// Qno:1
+// var caseQty;
+
+// Qno:2
+// caseQty="144";
+// alert(caseQty);
+
+// Qno:3
+// var num="9";
+
+// Qno:4
+// var sum;
+// sum=2+2;
+// alert(sum);
+
+// Qno:5
+// var merchTotal = 100;
+// var shippingCharge = 10;
+// var orderTotal = merchTotal + shippingCharge;
+// alert(orderTotal);
+
+// Qno:6
+// var number = 4;
+// number = number + 4;
+// alert(number);
+
+// ............................THE END ................................
+
